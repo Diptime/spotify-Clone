c3b745f708fc445dfda082b111d2b9601f5c03f0
Dipti <diptiimee@gmail.com>
2021-12-25 11:56:33 +0530

    loginUrl used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
-import './App.css';
-import Login from './Login';
+export default App;
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login/>
-    </div>
-  );
-}
+  const [token, setToken] = useState();
 
-export default App;
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
+}
